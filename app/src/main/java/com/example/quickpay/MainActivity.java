de5ad042f0bedbb43f2bbe1d5bd9a823d03cce38
Floyd <70218160+Rhi1o@users.noreply.github.com>
2022-03-29 22:50:18 -0500

    Edited MainActivity.java

--- app/src/main/java/com/example/quickpay/MainActivity.java
+++ app/src/main/java/com/example/quickpay/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.quickpay;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,19 +20,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_notifications);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        DBHandler dbHandler = new DBHandler(MainActivity.this);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        //dbHandler.addNewUser("Floyd Cain", "FC", "Hello");
+
+        Toast.makeText(MainActivity.this, "User added", Toast.LENGTH_SHORT).show();
     }
 
 }
