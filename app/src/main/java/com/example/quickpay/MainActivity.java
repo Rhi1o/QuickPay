bf91cdbaf2fec362ec6e5dfb73790e90e9599c25
Floyd <70218160+Rhi1o@users.noreply.github.com>
2022-04-08 23:30:56 -0500

    Menu popup started

--- app/src/main/java/com/example/quickpay/MainActivity.java
+++ app/src/main/java/com/example/quickpay/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.quickpay;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private DBHandler dbHandler;
+    private Button btnMenu;
     private TextView txtTransactionAmt;
     private Button btnCalcDelete;
     private Button btnCalcDecimal;
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnMenu = findViewById(R.id.btnMenu);
         txtTransactionAmt = findViewById(R.id.txtTransactionAmt);
         btnCalcDelete = findViewById(R.id.btnCalcDelete);
         btnCalcDecimal = findViewById(R.id.btnCalcDecimal);
@@ -48,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         dbHandler = new DBHandler(MainActivity.this);
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMenuPopup(v);
+            }
+        });
 
         btnCalcDelete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -202,4 +216,30 @@ public class MainActivity extends AppCompatActivity {
 
         txtTransactionAmt.setText(temp);
     }// End editText
+
+    public void showMenuPopup(View view) {
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_menu, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
 }// End main
