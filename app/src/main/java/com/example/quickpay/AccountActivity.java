6f716e6bb014cdadb35cd6a7f9b0fa0991edee97
Floyd <70218160+Rhi1o@users.noreply.github.com>
2022-04-21 22:22:04 -0500

    Removed Unnecessary imports

--- app/src/main/java/com/example/quickpay/AccountActivity.java
+++ app/src/main/java/com/example/quickpay/AccountActivity.java
@@ -2,26 +2,16 @@ package com.example.quickpay;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.quickpay.databinding.ActivityMainBinding;
+//import com.example.quickpay.databinding.ActivityMainBinding;
 
 import org.w3c.dom.Text;
 
--- app/src/main/java/com/example/quickpay/MainActivity.java
+++ app/src/main/java/com/example/quickpay/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.quickpay;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,9 +16,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.quickpay.databinding.ActivityMainBinding;
-
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
